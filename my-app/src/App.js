32df3bf4bcc3d65a1ac489e59ae431db2f614c29
John Traas <j.j.h.traas@gmail.com>
2021-09-22 15:17:05 +0200

    add import + render components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionalGreeting from './components/Schmololo.js';
+import StatefulGreeting from './components/StatefulGreeting';
+import Hatred from './components/Hatred';
+import HooksCounter from './components/HooksCounter';
 
 function App() {
   return (
@@ -9,6 +13,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <HooksCounter/>
+        <p> 
+          It works 
+        </p>
+        <p className="App-wololo">
+          <FunctionalGreeting name="&#128520;" />
+          </p>
+        <div className="App-likes">
+          <StatefulGreeting/>
+          <Hatred/>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
