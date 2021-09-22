f6b011f8dccc73c72956c7fb30bb1846c99fd28f
John Traas <j.j.h.traas@gmail.com>
2021-09-22 15:17:22 +0200

    add component HooksCounter

---
+++ my-app/src/components/HooksCounter.js
@@ -0,0 +1,16 @@
+import React, { useState }from "react";
+
+export default function HooksCounter(props) {
+    // count: a new state property
+    // setCount: a method to update count
+    // useState(0): initialize count to 0
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="greeting">
+            <h2>Hello {props.name}! I'm a functional component with hooks!</h2>
+            <h3>You've clicked this totally useless counter {count} times</h3>
+            <button onClick={() => setCount(count => count + 1)}>Increment Useless Counter</button>
+        </div>
+    )
+}
