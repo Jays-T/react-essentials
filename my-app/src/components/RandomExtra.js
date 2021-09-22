1b3f226edb7d1474ab9515cdc1bdac1254aa47a9
John Traas <j.j.h.traas@gmail.com>
2021-09-22 16:36:24 +0200

    add component RandomExtra

---
+++ my-app/src/components/RandomExtra.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default class RandomExtra extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: `😀`,
+            buttonText: `Don't click Me!`,
+        }
+    };
+
+    setDisplay = () => {
+        this.setState(prevState => {
+            return { 
+            display: prevState.display === `👎😠 ` ? `😀` : `👎😠 `,
+            buttonText: prevState.buttonText === `Don't click Me!` ? `Leave Angry Face Alone` : `Don't click Me!`}
+        });
+    };
+
+    render() {
+        return (
+            <div className="set-right">
+            <h2>{this.state.display}</h2>
+            <button class="affect-count-btn" onClick={this.setDisplay}>{this.state.buttonText}</button>
+            </div>
+        )
+    };
+}
